4468f3b4069d5c4beea0e93836b28fb553d7d01e
VorobyovDS <vorobyovds_it@mail.ru>
2018-02-04 14:54:04 +0300

    minus jquery

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,6 @@
  * Third party
  */
 //= ../../bower_components/vue/dist/vue.min.js
-//= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/clipboard/dist/clipboard.min.js
 var path = new Vue({
     el: '.method',
@@ -28,56 +27,72 @@ var path = new Vue({
 
 //------------------------------
 
-$(function () {
-    $(".method__input").on("click", function (e) {
-        $(this).addClass("method__input_revers");
-        $(this).siblings(".method__input-text").addClass("method__input-text_revers");
-    })
-});
 
-$(window).bind('scroll',function(e){
-    parallaxScroll();
-});
+function reversInput(Element) {
+     Element.classList.add('method__input_revers');
+     Element.previousElementSibling.classList.add('method__input-text_revers');
+
+};
+
+
+window.onscroll = function () {
+    parallaxScroll()
+};
+
 function parallaxScroll(){
     var SettingsScrolled = {
-        scrolled:$(window).scrollTop(),
+        scrolled:window.pageYOffset || document.documentElement.scrollTop,
         speed: 0.008,
+        elements: {
+            name:{
+            s1:document.getElementsByClassName('header__decoration_s1'),
+            s2:document.getElementsByClassName('header__decoration_s2'),
+            s3:document.getElementsByClassName('header__decoration_s3'),
+            s4:document.getElementsByClassName('header__decoration_s4'),
+            b1:document.getElementsByClassName('header__decoration_b1'),
+            b2:document.getElementsByClassName('header__decoration_b2')
+            },
+            main_settings_top:{
+                s1:43,
+                s2:7,
+                s3:18,
+                s4:33,
+                b1:30,
+                b2:11
+            }
+        },
         hightScroll:function () {
             var hight=SettingsScrolled.scrolled*SettingsScrolled.speed;
             if (hight>1) {
                 hight=1
             }
-            else {
-                hight=hight
-            }
             return hight
         }
     };
-    $('.header__decoration_s1').css('top',(43+(SettingsScrolled.hightScroll()))+'vh');
-    $('.header__decoration_s2').css('top',(7+(SettingsScrolled.hightScroll()))+'vh');
-    $('.header__decoration_s3').css('top',(18+(SettingsScrolled.hightScroll()))+'vh');
-    $('.header__decoration_s4').css('top',(33+(SettingsScrolled.hightScroll()))+'vh');
-    $('.header__decoration_b1').css('top',(30+(SettingsScrolled.hightScroll()))+'vh');
-    $('.header__decoration_b2').css('top',(11+(SettingsScrolled.hightScroll()))+'vh');
+    // ниже идет сверх ужасная вещь, не могу что-то сообразить как сделать лучше, нужно как то перебирать красиво значения
+    var i;
+    for(i=0; i<SettingsScrolled.elements.name.s1.length; i++)SettingsScrolled.elements.name.s1[i].style.top=SettingsScrolled.elements.main_settings_top.s1+SettingsScrolled.hightScroll()+'vh';
+    for(i=0; i<SettingsScrolled.elements.name.s2.length; i++)SettingsScrolled.elements.name.s2[i].style.top=SettingsScrolled.elements.main_settings_top.s2+SettingsScrolled.hightScroll()+'vh';
+    for(i=0; i<SettingsScrolled.elements.name.s3.length; i++)SettingsScrolled.elements.name.s3[i].style.top=SettingsScrolled.elements.main_settings_top.s3+SettingsScrolled.hightScroll()+'vh';
+    for(i=0; i<SettingsScrolled.elements.name.s4.length; i++)SettingsScrolled.elements.name.s4[i].style.top=SettingsScrolled.elements.main_settings_top.s4+SettingsScrolled.hightScroll()+'vh';
+    for(i=0; i<SettingsScrolled.elements.name.b1.length; i++)SettingsScrolled.elements.name.b1[i].style.top=SettingsScrolled.elements.main_settings_top.b1+SettingsScrolled.hightScroll()+'vh';
+    for(i=0; i<SettingsScrolled.elements.name.b2.length; i++)SettingsScrolled.elements.name.b2[i].style.top=SettingsScrolled.elements.main_settings_top.b2+SettingsScrolled.hightScroll()+'vh';
+
 };
 //-------------------копирование-в-буфер---Clipboard.js---------------------------
 
-$(function () {
-    $(".method__button").on("click", function (e) {
-        var clickId = $(this).prop('id');
-        var target = "#"+clickId;
-        var clipboard = new Clipboard(target);
-        clipboard.on('success', function(e) {
-            console.info('Action:', e.action);
-            console.info('Text:', e.text);
-            console.info('Trigger:', e.trigger);
-            e.clearSelection();
-        });
-        clipboard.on('error', function(e) {
-            console.error('Action:', e.action);
-            console.error('Trigger:', e.trigger);
-        });
-    })
-});
-
-
+function CopyButton (e) {
+    var clickId = e.id;
+    var target = "#"+clickId;
+    var clipboard = new Clipboard(target);
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        e.clearSelection();
+    });
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+};
